9758968f0f8c7360c1ec9ca184b5997bf8f766c1
Diego Ariel Morán <dmoran@sms-sudamerica.com>
2022-08-04 09:55:34 -0300

    logos fondos

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Index = () => {
 
   useEffect(() => {
 
-    document.body.style.backgroundImage='url(/images/fondo6.png)'
+    document.body.style.backgroundImage='url(/images/fondo5.png)'
    /* document.body.style.background='#2c5364'  W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
   });
 
